36d4289924d067150232a279e009a1b474b23fee
commit2main <229308316+commit2main@users.noreply.github.com>
2025-10-09 10:58:38 +0000

    feat(minimax): implement depth and move limitation for TTT minimax algorithm

--- minimax/tictactoe.py
+++ minimax/tictactoe.py
@@ -1,14 +1,17 @@
 import random
-from minimax.algo import MIN_VALUE, MAX_VALUE
 from logic.board import check_winner, find_empty_cells, print_board
+
+MIN_SCORE = -10
+MAX_SCORE = 10
+MAX_DEPTH = 1
 
 AI_PLAYER = "O"
 HUMAN_PLAYER = "X"
 
 TEST_BOARD: list[list[str]] = [
-    ["X", "", "X"],
-    ["X", "O", ""],
-    ["O", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
 ]
 TOTAL_CELLS = sum(len(row) for row in TEST_BOARD)
 
@@ -17,8 +20,9 @@ def minimax_ttt(
     board: list[list[str]],
     depth: int,
     is_max: bool = True,
-    alpha: int = MIN_VALUE,
-    beta: int = MAX_VALUE,
+    alpha: int = MIN_SCORE,
+    beta: int = MAX_SCORE,
+    limited: bool = False,
 ) -> int:
     """
     Minimax evaluation for Tic-Tac-Toe with alpha-beta pruning.
@@ -32,10 +36,16 @@ def minimax_ttt(
         is_max: True when it's the AI player's turn
         alpha: best found option for the AI player
         beta:  best found option for the human player
+        limited: if True, limit depth to MAX_DEPTH to improve performance
+            and introduce some inaccuracy
 
     Returns:
         int: minimax score for the current board state
     """
+    # Limit search depth to improve performance if specified
+    if limited and depth >= MAX_DEPTH:
+        return 0
+
     winner = check_winner(board)
     # Depth is used to prefer faster wins and slower losses
     if winner == AI_PLAYER:
@@ -49,7 +59,7 @@ def minimax_ttt(
     moves = find_empty_cells(board)
 
     if is_max:
-        highest_score = MIN_VALUE
+        highest_score = MIN_SCORE
         # Try all possible moves and choose the one with the highest score
         for i, j in moves:
             # Make a move and call minimax recursively, then undo the move
@@ -65,7 +75,7 @@ def minimax_ttt(
                 break
         return highest_score
     else:
-        lowest_score = MAX_VALUE
+        lowest_score = MAX_SCORE
         # Try all possible moves and choose the one with the lowest score
         for i, j in moves:
             # Make a move and call minimax recursively, then undo the move
@@ -98,22 +108,30 @@ def random_move(board: list[list[str]]) -> tuple[int, int]:
     return random.choice(empty_cells)
 
 
-def best_move(board: list[list[str]]) -> tuple[int, int]:
+def ai_move(board: list[list[str]], limited: bool = False) -> tuple[int, int]:
     """
-    Select the best move for the AI player using the minimax algorithm.
+    Selects a move for the AI player using the minimax algorithm.
 
     Args:
         board: NxN Tic-Tac-Toe board (list of lists of str)
+        limited: if True, only compute for 2 moves to improve performance
+            performance and introduce some inaccuracy
     Returns:
         tuple[int, int]: coordinates of the best move
     """
-    best_score = MIN_VALUE
+    best_score = MIN_SCORE
     best_move = (-1, -1)
 
     # Find all possible moves (empty cells) on the board
     moves = find_empty_cells(board)
     if not moves:
         raise ValueError("No empty cells available for a move.")
+    if len(moves) == 1:
+        # Return the only available move
+        return moves[0]
+
+    if limited:
+        moves = random.sample(moves, 2)
 
     # Try all possible moves and choose the one with the best score
     for i, j in moves:
@@ -121,7 +139,7 @@ def best_move(board: list[list[str]]) -> tuple[int, int]:
         sim_board = [row[:] for row in board]
         sim_board[i][j] = AI_PLAYER
         # Get the minimax score for this move
-        score = minimax_ttt(sim_board, 0, False)
+        score = minimax_ttt(sim_board, 0, False, limited=limited)
         # Update to the best score found so far
         if score > best_score:
             best_score = score
@@ -140,8 +158,8 @@ if __name__ == "__main__":
     print_board(TEST_BOARD)
     TEST_BOARD[move[0]][move[1]] = ""
 
-    print(f"Best move (player {AI_PLAYER}):")
-    move = best_move(TEST_BOARD)
+    print(f"Minimax move (player {AI_PLAYER}):")
+    move = ai_move(TEST_BOARD, limited=True)
     TEST_BOARD[move[0]][move[1]] = AI_PLAYER
     print_board(TEST_BOARD)
     TEST_BOARD[move[0]][move[1]] = ""
