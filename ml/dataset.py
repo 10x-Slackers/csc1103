17acdbcb34e174f4fa38b0909613dd0d5dff59cf
commit2main <229308316+commit2main@users.noreply.github.com>
2025-10-16 02:59:30 +0000

    feat(ml): implement dataset parsing in Python

---
+++ ml/dataset.py
@@ -0,0 +1,83 @@
+from typing import TypedDict
+
+from logic.board import print_board
+
+# Dataset file must be placed in the root of the project in the "dataset" folder
+DATASET_PATH = "dataset/tic-tac-toe.data"
+
+
+class Entry(TypedDict):
+    """
+    Represents a single entry in the Tic Tac Toe dataset.
+
+    Attributes:
+        board: 3x3 Tic-Tac-Toe board (list of lists of str)
+        outcome: Result of the game ("Positive" or "Negative")
+    """
+
+    board: list[list[str]]
+    outcome: str
+
+
+def main() -> None:
+    """
+    Main function to demonstrate dataset parsing. Prints the first 5 entries.
+    """
+    for entry in parse_dataset(DATASET_PATH)[:5]:
+        print_board(entry.get("board"))
+        print("Outcome:", entry.get("outcome"))
+        print()
+
+
+def read_dataset(file_path: str) -> list[list[str]]:
+    """
+    Read the provided dataset file and return its contents as a list of rows.
+
+    Args:
+        file_path: Path to the dataset file
+    Returns:
+        List of rows, where each row is a list of strings
+    """
+    with open(file_path) as file:
+        return [row.strip().split(",") for row in file.readlines()]
+
+
+def convert_ttt_matrix(ttt: list[str], formatted: bool = False) -> list[list[str]]:
+    """
+    Convert a flat list into a 3x3 matrix, optionally formatting the cells to fit
+    our board representation.
+
+    Args:
+        ttt: Flat list of 9 elements representing the board
+        formatted: Whether to format the cells (e.g., uppercase "X" and "O")
+    Returns:
+        3x3 matrix (list of lists)
+    """
+    if formatted:
+        ttt = [cell.upper() if cell in ("x", "o") else " " for cell in ttt]
+
+    return [ttt[i : i + 3] for i in range(0, 9, 3)]
+
+
+def parse_dataset(file_path: str) -> list[Entry]:
+    """
+    Read and parse the dataset file and convert it into our Entry format.
+
+    Args:
+        file_path: Path to the dataset file
+    Returns:
+        List of Entry objects containing the board and outcome
+    """
+    dataset: list[Entry] = []
+
+    for row in read_dataset(file_path):
+        ttt = row[:-1]
+        outcome = row[-1]
+        board = convert_ttt_matrix(ttt, formatted=True)
+        dataset.append({"board": board, "outcome": outcome})
+
+    return dataset
+
+
+if __name__ == "__main__":
+    main()
