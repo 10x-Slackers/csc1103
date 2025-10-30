bba8878a87aa636ced1502c38ce15ad9021b9156
Jerome-4469 <2500658@sit.singaporetech.edu.sg>
2025-10-30 16:12:10 +0800

    feat(gui): Merging Files Together (#36)
    Initial Set of Codes

---
+++ gui/main.py
@@ -0,0 +1,16 @@
+import gi
+
+gi.require_version("Gtk", "4.0")
+from gi.repository import Gtk  # noqa
+
+from ui import main_menu  # noqa
+
+
+def on_activate(app):
+    main_menu.main_menu(app)
+
+
+if __name__ == "__main__":
+    app = Gtk.Application(application_id="com.csc1103.TicTacToeMenu")
+    app.connect("activate", on_activate)
+    app.run()
