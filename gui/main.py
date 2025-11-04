2ac86de6eae5c3a47de6b98552612858cfa66930
Jerome-4469 <2500658@sit.singaporetech.edu.sg>
2025-11-04 14:40:08 +0800

    feat(gui): Final Features Addition (#39)
    * Initial Codes (All features included)
    
    * Edited with Copilot Comments
    
    * Minor Comment Edits

--- gui/main.py
+++ gui/main.py
@@ -2,12 +2,11 @@ import gi
 
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk  # noqa
-
 from ui import main_menu  # noqa
 
 
 def on_activate(app):
-    main_menu.main_menu(app)
+    main_menu.main(app)
 
 
 if __name__ == "__main__":
