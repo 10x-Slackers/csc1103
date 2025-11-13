3393207216e3f46e19d70fb1b793a109daad4576
commit2main <229308316+commit2main@users.noreply.github.com>
2025-11-13 17:26:18 +0000

    implement test program to test minimax and naive bayes

---
+++ src/app/main.c
@@ -0,0 +1,51 @@
+
+#include "../shared/board.h"
+#include "../shared/minimax.h"
+#include "../shared/naive_bayes.h"
+
+int main() {
+  NaiveBayesModel model;
+  int res = load_nb_model(&model, "src/ml_trainer/weights.bin");
+  if (res != 0) {
+    printf("Error loading model\n");
+    return 1;
+  }
+
+  Board board;
+  init_board(&board, PLAYER_X);
+  // Let Player X make a random first move
+  Cell rand_move = random_move(&board);
+  make_move(&board, &rand_move);
+  // Let Player O make a random second move
+  rand_move = random_move(&board);
+  make_move(&board, &rand_move);
+
+  while (board.move_count < SIZE * SIZE) {
+    print_board(&board);
+    printf("\n");
+
+    if (board.current_player == PLAYER_X) {
+      Cell ai_move = minimax_find_move(&board, true);
+      make_move(&board, &ai_move);
+    } else {
+      // Cell ai_move = random_move(&board);
+      Cell ai_move = nb_find_move(&board, &model);
+      make_move(&board, &ai_move);
+    }
+
+    Winner result = check_winner(&board);
+    if (result != ONGOING) {
+      print_board(&board);
+      if (result == WIN_X) {
+        printf("Player X wins!\n");
+      } else if (result == WIN_O) {
+        printf("Player O wins!\n");
+      } else {
+        printf("It's a draw!\n");
+      }
+      break;
+    }
+  }
+
+  return 0;
+}
