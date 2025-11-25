5fac809bd86cc655b63c687663932bc99d3ae8fd
commit2main <229308316+commit2main@users.noreply.github.com>
2025-11-25 16:18:54 +0000

    real fix bug

--- src/app/main_menu.h
+++ src/app/main_menu.h
@@ -3,8 +3,8 @@
 
 #include <gtk/gtk.h>
 
-#define AUDIO_ON_RESOURCE "/org/csc1103/tictactoe/volume-up-rounded.svg"
-#define AUDIO_OFF_RESOURCE "/org/csc1103/tictactoe/volume-off-rounded.svg"
+#define AUDIO_ON_RESOURCE "/org/csc1103/tictactoe/icons/volume-up-rounded.svg"
+#define AUDIO_OFF_RESOURCE "/org/csc1103/tictactoe/icons/volume-off-rounded.svg"
 
 /**
  * @brief Initialise main menu UI and connect signals.
